f7f05c3cd27bab2e7307bbd3d4912f5e3bba6d4b
Hanane Feg <hananefegrachi@gmail.com>
2023-05-06 11:21:34 +0200

    Creation des fichiers de database

---
+++ app/Http/Controllers/ServeursController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Serveurs;
+use App\Http\Requests\StoreServeursRequest;
+use App\Http\Requests\UpdateServeursRequest;
+
+class ServeursController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreServeursRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Serveurs $serveurs)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Serveurs $serveurs)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateServeursRequest $request, Serveurs $serveurs)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Serveurs $serveurs)
+    {
+        //
+    }
+}
